d7ecb05a19e6b45924c6aecaafafa0015c7e9b1d
JJ Ryan <jryan@motor.com>
2017-07-25 22:32:12 -0400

    initial commit

---
+++ Lab 20/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab_20.Models;
+
+
+namespace Lab_20.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult About (Registration r)
+        {
+
+            ViewBag.Welcome = "Hello from me, " + r.name + ". Is "+ r.email+ " correct?";
+            return View(r);
+
+        }
+
+    }
+
+}
---
+++ Lab 20/Models/Registration Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+//using System.Data.Entity;
+
+namespace Lab_20.Models
+{
+    public class Registration
+    {
+        [Required]
+        [Key]
+        public string name { get; set; }
+
+        [Required]
+        public string email { get; set; }
+    }
+
+    //public class DBRegisterContext : DbContext
+    //{
+    //    public DbSet<Register> Entries { get; set; }
+
+    //    public System.Data.Entity.DbSet<Lab20.Models.Item> Items { get; set; }
+    //}
+}
+
